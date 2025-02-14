c1cd0f2acdffd7f5c76b31681d8eb592293dd7c4
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-14 18:48:39 +0000

    added some examples of vuv changes

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,5 +1,9 @@
 varying vec2 vUv;
 void main()
 {
-    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
+    //gl_FragColor = vec4(vUv, 1.0, 1.0);
+    //gl_FragColor = vec4(vUv, 0.0, 1.0);
+    //gl_FragColor = vec4(vUv, 0.7, 1.0);
+    //gl_FragColor = vec4(vUv.x,vUv.x, vUv.x, 1.0);
+    gl_FragColor = vec4(vUv.x,vUv.y, vUv.x, 1.0);
 }
