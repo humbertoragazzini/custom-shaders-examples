72389a702b873fef124cb860915292b8622f8d6e
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-15 12:35:43 +0000

    added pattern 9

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -27,9 +27,12 @@ void main()
    
     //Pattern 7
     //float strength = mod(vUv.y * 10.0, 1.0 );
+     
+    //Pattern 8
+    //float strength = step(0.5,mod(vUv.y * 10.0, 1.0 ));
     
     //Pattern 8
-    float strength = step(0.5,mod(vUv.y * 10.0, 1.0 ));
+    float strength = step(0.8,mod(vUv.y * 10.0, 1.0 ));
     
     gl_FragColor = vec4(strength,strength,strength, 1.0);
 }
