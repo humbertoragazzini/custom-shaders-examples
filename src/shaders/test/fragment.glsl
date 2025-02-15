b4088d5ca9ddf9e4e9915890b050d845efbd2ed9
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-15 11:47:50 +0000

    starting with the different patterns

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,13 +1,20 @@
 varying vec2 vUv;
 void main()
 {
+    //pattern custom beto
     //gl_FragColor = vec4(vUv, 1.0, 1.0);
     //gl_FragColor = vec4(vUv, 0.0, 1.0);
     //gl_FragColor = vec4(vUv, 0.7, 1.0);
+
+    //pattern custom beto 2
     //gl_FragColor = vec4(vUv.x,vUv.x, vUv.x, 1.0);
     //float r = abs(sin(vUv.y*6.28))*abs(cos(vUv.x*6.28));
     //float g = abs(sin(vUv.x*6.28))*abs(cos(vUv.y*6.28));
     //float b = abs(sin(vUv.y*6.28))*abs(cos(vUv.x*6.28));
     //gl_FragColor = vec4(r,g,b, 1.0);
-    gl_FragColor = vec4(vUv,0.7, 1.0);
+
+    //Pattern 3
+    float strength = vUv.x;
+
+    gl_FragColor = vec4(strength,strength,strength, 1.0);
 }
