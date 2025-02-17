762dae770003e88625c90cf122a522719605c9a3
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-17 20:58:31 +0000

    added shader from bruno

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -70,5 +70,10 @@ void main()
     //Pattern 19
     // this is my float strength =  step(0.2,max(abs(vUv.y - 0.5), abs(vUv.x - 0.5))) - step(0.25,max(abs(vUv.y - 0.5), abs(vUv.x - 0.5)));
 
+    float square1 = step(0.2,max(abs(vUv.y - 0.5), abs(vUv.x - 0.5)));
+    float square2 = 1.0 -  step(0.25,max(abs(vUv.y - 0.5), abs(vUv.x - 0.5)));
+
+    float strength = square1 * square2;
+
     gl_FragColor = vec4(strength,strength,strength, 1.0);
 }
