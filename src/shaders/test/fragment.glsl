23569bc370f0c477340a7a673c0dfe0c6df0b87e
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-20 21:06:50 +0000

    added pattern 38

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -173,7 +173,7 @@ void main()
       
     //pattern 37 bruno shader
     vec2 waveUv = vec2(
-                vUv.x,
+                vUv.x + sin(vUv.y*20.0) * 0.15,
                 vUv.y + sin(vUv.x*20.0) * 0.15
             );
     float strength = 1.0 - step(0.01, abs(distance(waveUv, vec2(0.5))-0.25));    
