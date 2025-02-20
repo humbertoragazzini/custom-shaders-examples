9638b9f48e2a48f06a9156787afe4268db2689a4
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-20 21:01:34 +0000

    added pattern 37 from bruno

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -172,7 +172,11 @@ void main()
     //float strength = 1.0 - (1.0 - step(abs(distance(vUv,vec2(0.5,0.1*sin(vUv.x*20.0)+0.5))-0.30),0.015));
       
     //pattern 37 bruno shader
-    
+    vec2 waveUv = vec2(
+                vUv.x,
+                vUv.y + sin(vUv.x*20.0) * 0.15
+            );
+    float strength = 1.0 - step(0.01, abs(distance(waveUv, vec2(0.5))-0.25));    
 
     gl_FragColor = vec4(strength,strength,strength, 1.0);
 }
