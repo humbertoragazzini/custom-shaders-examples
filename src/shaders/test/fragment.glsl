9b60acf76b70172c5d35fbc07bf424efb9555617
humbertoragazzini <beto_vicio@hotmail.com>
2025-02-21 20:50:04 +0000

    added partial pattern 45

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -213,11 +213,12 @@ void main()
 
 
     // pattern 44
-    float angle = atan(vUv.x-0.5,vUv.y-0.5);
+    float angle = atan(vUv.x - 0.5, vUv.y - 0.5 );
     angle /= PI * 2.0;
     angle += 0.5;
-    angle = sin(angle*50.0);
-    float strength = (1.0 - step(0.011,abs(distance(vUv, vec2(0.5)) - 0.25)));
+    float sinusoid = sin(angle *100.0);
+    float radius = 0.25 + sinusoid * 0.02;
+    float strength = (1.0 - step(0.011,abs(distance(vUv, vec2(0.5)) - radius)));
 
     gl_FragColor = vec4(strength,strength,strength, 1.0);
 }
